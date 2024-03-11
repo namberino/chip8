35e4605b742f130838deebf1a63f0a41190a8193
namber0 <namnb2402@gmail.com>
2024-03-11 15:56:55 +0700

    Remove test file

--- test.sh
+++
@@ -1,9 +0,0 @@
-if [ -f "obj/chip8.o" ]; then exit 0; else exit 1; fi
-if [ -f "obj/periph.o" ]; then exit 0; else exit 1; fi
-if [ -f "obj/main.o" ]; then exit 0; else exit 1; fi
-
-if [ ./bin/chip8 -eq 1 ]; then exit 0; fi
-
-./bin/chip8 roms/opcode-test.ch8
-./bin/chip8 roms/maze.ch8
-./bin/chip8 roms/particle.ch8
